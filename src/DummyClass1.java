dc60d03cd168fa25a27168d08cbf6f446bc3e6f3
kamrul <kamrul4h3190@gmail.com>
2025-02-22 01:13:39 +0600

    leetcode 27

--- src/DummyClass1.java
+++ src/DummyClass1.java
@@ -2,59 +2,122 @@ import java.util.Arrays;
 
 public class DummyClass1 {
     public static void main(String[] args) {
-//        int [] nums1 = {1,2,3,0,0,0};
-//        int [] nums2 = {2,5,6};
+        int [] nums = {0,1,2,2,3,0,4,2}; int val=2;
+//        int [] nums = {3,2,2,3}; int val=3;
 
-        int [] nums2 = {4,5,6,6};
-        int [] nums1 = {3,4,6,0,0,0,0};
-
-//        int [] nums1 = {3,4,6};
-//        int [] nums2 = {4,5,6,6};
-
-        int[] merged = merge2(nums1,nums2, nums1.length- nums2.length, nums2.length);
-        System.out.println(Arrays.toString(Arrays.stream(merged).toArray()));
+//        int [] nums = {2,2,3}; int val=2;
+//        int [] nums = {4,5}; int val=9;
+//        int [] nums = {1}; int val=1;
+        int[] removed = removeElement3(nums,val);
+        System.out.println(Arrays.toString(Arrays.stream(removed).toArray()));
     }
 
-    //merging two sorted arrays using a third array.
-    public  static int[] merge(int[] nums1,int[] nums2,int m, int n){
-        int[] numsMerged = new int[m+n];
 
-        int i=0,j=0,k=0;
-        for (; i < numsMerged.length && j<m && k<n; i++) {
+//    public static int[] removeElement(int[] nums, int val) {
+//        int l=0;
+//
+//        for (int r = 0; r < nums.length; r++) {
+//            if(nums[r]!=val){
+//                nums[l] = nums[r];
+//                l++;
+//            }
+//        }
+//        System.out.println("l : "+l);
+//
+//        return nums;
+//    }
+//public static int[] removeElement(int[] nums, int val) {
+//    int n= nums.length;
+//    int l=0,r=n-1;
+//    while (l<=r){
+//        if (nums[l]==val && nums[r]==val){
+//            r--;
+//        }
+//        else if(nums[l]!=val && nums[r]==val){
+//            l++;r--;
+//        } else if (nums[l]!=val && nums[r]!=val) {
+//            l++;
+//        } else if(nums[l]==val && nums[r]!=val){
+//            nums[l] = nums[r];
+//            l++;r--;
+//        }
+//    }
+//
+//    System.out.println("l : "+(l));
+//
+//    return nums;
+//    }
 
-            if (nums1[j] < nums2[k]){
-                numsMerged[i] = nums1[j];
-                j++;
-            }else{
-                numsMerged[i] = nums2[k];
-                k++;
+    public static int[] removeElement2(int[] nums, int val) {
+        int n= nums.length;
+        int l=0,r=n-1;
+        while (l<=r){
+            if (nums[l]==val && nums[r]==val){
+                r--;
+            }
+            else if(nums[l]!=val && nums[r]==val){
+                l++;r--;
+            } else if (nums[l]!=val && nums[r]!=val) {
+                l++;
+            } else if(nums[l]==val && nums[r]!=val){
+                nums[l] = nums[r];
+                l++;r--;
             }
         }
 
-        while (k<n) {numsMerged[i++] = nums2[k];k++;}
-        while (j<m) {numsMerged[i++] = nums1[j];j++;}
-        return numsMerged;
+        System.out.println("l : "+(l));
+
+        return nums;
     }
 
-    //merging two sorted arrays without extra space.
-    public  static int[] merge2(int[] nums1,int[] nums2,int m, int n){
-
-        int i=m+n-1,j=m-1,k=n-1;
-        for (;i>=0 && (j>=0 && k>=0 ) ;i--) {
-            if (nums2[k]>nums1[j]){
-                nums1[i] = nums2[k];
-                k--;
-            }else {
-                nums1[i] = nums1[j];
-                j--;
-            }
+//    public static int[] removeElement3(int[] nums, int val) {
+//        int n= nums.length;
+//        int l=0,r=n-1;
+//        while (l<=r){
+//            while(nums[l]!=val && l!=r){
+//                l++;
+//            }
+//            while(nums[r]==val && l!=r){
+//                r--;
+//            }
+//
+//            if(nums[l]==val && nums[r]!=val){
+//                nums[l] = nums[r];
+//                l++;r--;
+//            }
+//            else if(nums[l]!=val && nums[r]!=val){
+//                l++;
+//            }
+//            else if(nums[l]==val && nums[r]==val){
+//                r--;
+//            }
+//        }
+//        System.out.println("l : "+l);
+//        System.out.println("r : "+r);
+//        System.out.println("l-r : "+(l-r));
+//
+//        return nums;
+//    }
+public static int[] removeElement3(int[] nums, int val) {
+    int n= nums.length;
+    int l=0,r=n-1;
+    while (l<=r){
+        while((nums[l]==val && nums[r]!=val)){
+            r--;
         }
-        System.out.println("j : "+j);
-        System.out.println("k : "+k);
+        while((nums[l]!=val && nums[r]!=val)){
+            l++;
+        }
 
-        while (j>=0){nums1[i--]=nums1[j];j--;}
-        while (k>=0){nums1[i--]=nums2[k];k--;}
-
-        return nums1;
+        if(nums[l]==val && nums[r]!=val){
+            nums[l] = nums[r];
+            l++;r--;
+        }
     }
+    System.out.println("l : "+l);
+    System.out.println("r : "+r);
+    System.out.println("l-r : "+(l-r));
+
+    return nums;
+}
 }
