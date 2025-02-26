92d91749dd559510877b0b230565c8d8c1ece55c
kamrul <kamrul4h3190@gmail.com>
2025-02-26 14:12:59 +0600

    leetcode 169

--- src/DummyClass1.java
+++ src/DummyClass1.java
@@ -1,35 +1,52 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class DummyClass1 {
     public static void main(String[] args) {
-//        int [] nums = {0,0,1,1,1,2,2,3,3,4};
-//        int [] nums = {1,1,2};
+//        int [] nums = {3,2,3};
+//        int [] nums = {2,2,1,1,1,2,2};
+        int [] nums = {1,1,2,2,3,3,3,4,4,4,1,1,1};
 
-        int [] nums = {2,2};
-//        int [] nums = {4,5}; int val=9;
-//        int [] nums = {1}; int val=1;
-        int[] removed = removeDuplicates(nums);
-        System.out.println(Arrays.toString(Arrays.stream(removed).toArray()));
+
+        int majorityElement = majorityElement2(nums);
+        System.out.println("M E : "+majorityElement);
     }
 
 
 
+    //o(n) naive approach
+    public static int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> freqMap = new HashMap<>();
+        int me = 0;
 
-    public static int[] removeDuplicates(int[] nums) {
-        int n= nums.length;
-        int l=0,r=0;
-        for (; r < n; r++) {
-            if(nums[r]!=nums[l]){
-                nums[l+1]=nums[r];
-                l++;
+        for (int n : nums) {
+            if (!freqMap.containsKey(n)){
+                freqMap.put(n,1);
+            }else {
+                freqMap.put(n,freqMap.get(n)+1);
+            }
+            if (freqMap.get(n)> nums.length/2){
+                me=n;
             }
         }
-
-        System.out.println("l : "+(l+1));
-
-        return nums;
+        return me;
     }
+    //moor's voting algorithm
+    public static int majorityElement2(int[] nums) {
+        int me = nums[0],point=0;
 
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i]==me){
+                point++;
+            }else {
+                point--;
+            }
 
+            if (point==0){
+                me=nums[i+1];
+            }
+        }
+        return me;
+    }
 
 }
