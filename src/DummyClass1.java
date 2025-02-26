7470eb6785b16697083101084a895039b2cc5720
kamrul <kamrul4h3190@gmail.com>
2025-02-27 00:11:18 +0600

    leetcode 121

--- src/DummyClass1.java
+++ src/DummyClass1.java
@@ -3,50 +3,28 @@ import java.util.HashMap;
 
 public class DummyClass1 {
     public static void main(String[] args) {
-//        int [] nums = {3,2,3};
-//        int [] nums = {2,2,1,1,1,2,2};
-        int [] nums = {1,1,2,2,3,3,3,4,4,4,1,1,1};
+        int [] prices = {7,6,4,3,1};
+//        int prices[] = {2,1,2,1,0,1,2};
 
 
-        int majorityElement = majorityElement2(nums);
-        System.out.println("M E : "+majorityElement);
+        int maxProfit = maxProfit(prices);
+        System.out.println("Max Profit : "+maxProfit);
     }
 
 
 
-    //o(n) naive approach
-    public static int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> freqMap = new HashMap<>();
-        int me = 0;
+    public static int maxProfit(int[] prices) {
 
-        for (int n : nums) {
-            if (!freqMap.containsKey(n)){
-                freqMap.put(n,1);
-            }else {
-                freqMap.put(n,freqMap.get(n)+1);
+        int lowestBuyingPrice = Integer.MAX_VALUE,maxProfit = Integer.MIN_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < lowestBuyingPrice){
+                lowestBuyingPrice = prices[i];
             }
-            if (freqMap.get(n)> nums.length/2){
-                me=n;
+            if (prices[i]-lowestBuyingPrice>maxProfit){
+                maxProfit = prices[i]-lowestBuyingPrice;
             }
         }
-        return me;
-    }
-    //moor's voting algorithm
-    public static int majorityElement2(int[] nums) {
-        int me = nums[0],point=0;
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i]==me){
-                point++;
-            }else {
-                point--;
-            }
-
-            if (point==0){
-                me=nums[i+1];
-            }
-        }
-        return me;
+        return maxProfit;
     }
 
 }
