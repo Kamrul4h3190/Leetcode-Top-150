fa062bb64186af5a1b6a7627f1f05a345f6cce51
kamrul <kamrul4h3190@gmail.com>
2025-03-02 03:12:50 +0600

    leetcode 58

--- src/DummyClass1.java
+++ src/DummyClass1.java
@@ -4,41 +4,24 @@ import java.util.HashMap;
 public class DummyClass1 {
     public static void main(String[] args) {
 //        String s = "XIX";
-        String s = "MCMXCIV";
+        String s = "   fly me   to   the moon  ";
 
-        int intValue = romanToInt(s);
-        System.out.println("roman to int : "+intValue);
+        int lastWordLength = lengthOfLastWord(s);
+        System.out.println("lastWordLength : "+lastWordLength);
     }
 
 
 
-    public static int romanToInt(String s) {
-
-        HashMap<Character,Integer> valueMap = new HashMap<>();
-        valueMap.put('I',1);
-        valueMap.put('V',5);
-        valueMap.put('X',10);
-        valueMap.put('L',50);
-        valueMap.put('C',100);
-        valueMap.put('D',500);
-        valueMap.put('M',1000);
-
-        int roman = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-
-            if(i+1>=s.length()){
-                return roman + valueMap.get(s.charAt(i));
-            }
-            else if (valueMap.get(s.charAt(i)) >= valueMap.get(s.charAt(i+1))){
-                roman+=valueMap.get(s.charAt(i));
-            }else if(valueMap.get(s.charAt(i)) < valueMap.get(s.charAt(i+1))){
-                roman+= valueMap.get(s.charAt(i+1))-valueMap.get(s.charAt(i));
-                i++;
-            }
+    public static int lengthOfLastWord(String s) {
+        int length = 0;
+        for (int i = s.length()-1; i >=0 ; i--) {
+            if (s.charAt(i)!=' ')
+                length++;
+            else if (s.charAt(i)==' ' && length>0)
+                break;
         }
 
-        return roman;
+        return length;
     }
 
 }
