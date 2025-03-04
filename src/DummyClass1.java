b1162c29646bd312de29cfd2bded348a4682aa00
kamrul <kamrul4h3190@gmail.com>
2025-03-04 15:43:04 +0600

    leetcode 14

--- src/DummyClass1.java
+++ src/DummyClass1.java
@@ -3,25 +3,73 @@ import java.util.HashMap;
 
 public class DummyClass1 {
     public static void main(String[] args) {
-//        String s = "XIX";
-        String s = "   fly me   to   the moon  ";
+        String[] strs = {"aac","acab","aa","abba","aa"};
+//        String[] strs = {"flower","flower","flower","flower"};
+//        String[] strs = {"flower","flow","flight"};
+//        String[] strs = {"flower","flow"};
+//        String[] strs = {"aaa","aa","aaa"};
+//        String[] strs = {"cir","car"};
+//        String[] strs = {"aba","c","b","a","ab"};
+//        String[] strs = {"ab", "a"};
 
-        int lastWordLength = lengthOfLastWord(s);
-        System.out.println("lastWordLength : "+lastWordLength);
+        String prefix = longestCommonPrefix3(strs);
+        System.out.println("prefix : "+prefix);
     }
 
 
 
-    public static int lengthOfLastWord(String s) {
-        int length = 0;
-        for (int i = s.length()-1; i >=0 ; i--) {
-            if (s.charAt(i)!=' ')
-                length++;
-            else if (s.charAt(i)==' ' && length>0)
+//    public static String longestCommonPrefix(String[] strs) {
+//        String prefix = strs[0];
+//        if (prefix.isEmpty()) return "";
+//        if (strs.length==1) return prefix;
+//
+//        int stopIndex =prefix.length();
+//        for (String word : strs) {
+//
+//
+//            if (prefix.startsWith(word) && word.length()<prefix.length() && word.length() < stopIndex){
+//                stopIndex = word.length();
+//            } else if (word.charAt(0)!=prefix.charAt(0)) {
+//                return "";
+//            } else {
+//                for (int i = 0; i<prefix.length(); i++) {
+//                    if (word.charAt(i)!=prefix.charAt(i)){
+//                        stopIndex = i; break;
+//                    }
+//                }
+//            }
+//
+//
+//        }
+//
+//        return prefix.substring(0,stopIndex);
+//    }
+
+    public static String longestCommonPrefix2(String[] strs) {
+        StringBuilder prefix = new StringBuilder();
+        Arrays.sort(strs);
+
+        String firstWord = strs[0];
+        String lastWord = strs[strs.length-1];
+
+        for (int i = 0; i < firstWord.length(); i++) {
+            if (firstWord.charAt(i)!=lastWord.charAt(i))
                 break;
+            prefix.append(firstWord.charAt(i));
         }
 
-        return length;
+        return prefix.toString();
+    }
+    public static String longestCommonPrefix3(String[] strs) {
+
+        String prefix = strs[0];
+        for (int i = 1; i <strs.length ; i++) {
+            while (!(strs[i]).startsWith(prefix)){
+                prefix = prefix.substring(0,prefix.length()-1);
+            }
+        }
+
+        return prefix;
     }
 
 }
