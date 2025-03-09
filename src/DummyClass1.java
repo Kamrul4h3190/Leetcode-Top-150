6568b04f2ea0af36e36876139a1c6949045f613f
kamrul <kamrul4h3190@gmail.com>
2025-03-10 01:55:04 +0600

    leetcode 80

--- src/DummyClass1.java
+++ src/DummyClass1.java
@@ -3,19 +3,64 @@ import java.util.HashMap;
 
 public class DummyClass1 {
     public static void main(String[] args) {
-//        String haystack = "sadbutsad",needle = "sad";
-//        String haystack = "leetcode", needle = "leeto";
-        String haystack = "leetcode", needle = "tcode";
-        int needleIndex = strStr(haystack,needle);
-        System.out.println("index : "+needleIndex);
+//        int [] nums = {1,1,1,2,3};
+//        int [] nums = {0,0,1,1,1,2,2,3,3,4};
+//        int[] nums = {0,0,1,1,1,1,2,3,3};
+        int [] nums = {1,1,1,1,2,3,3,3};
+//        int [] nums = {1,1,1,2,2,3};
+//        int [] nums = {1,1,1,2,2,2,3,3};
+//        int [] nums = {1,1,1};
+        int endPositin = removeDuplicate5(nums);
+        System.out.println("after remove : "+ Arrays.toString(nums));
+        System.out.println("end position : "+endPositin);
     }
-    public static int strStr(String haystack, String needle) {
-        for (int i = 0; i <= haystack.length()-needle.length(); i++) {
-            if (haystack.substring(i).startsWith(needle)){
-                return i;
+    public static int removeDuplicate5(int[] nums) {
+
+        if(nums.length<3) return nums.length;
+        int i,p=2;
+        for ( i = 2; i < nums.length;i++ ) {
+
+            if (nums[i]!=nums[p] && nums[i]>nums[p-2]){
+                nums[p]=nums[i];
+                p++;
+            }
+            else if (nums[i]!=nums[p-2])
+                p++;
+        }
+        return p;
+    }
+
+
+
+    public static int removeDuplicates(int[] nums) {
+
+        int i,j=0;
+        for ( i = 1; i < nums.length; ) {
+            while (nums[j]==nums[i-1] && j+1<nums.length){
+                j++;
+            }
+
+            if (nums[i]<=nums[i-1]  &&nums[j]!=nums[i-1]){
+                nums[i]=nums[j];
+                i++;
+            }
+            if (nums[i]>nums[i-1]) i++;
+            if (j== nums.length-1) break;
+        }
+        return i;
+    }
+
+
+    public static int removeDuplicates2(int[] nums) {
+        int n= nums.length;
+        int l=0,r=0;
+        for (; r < n; r++) {
+            if(nums[r]!=nums[l]){
+                nums[l+1]=nums[r];
+                l++;
             }
         }
-        return -1;
+        return l+1;
     }
 
 }
