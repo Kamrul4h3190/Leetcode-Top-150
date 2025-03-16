74c934296ab57d68612a62187d3159f3485f9c35
kamrul <kamrul4h3190@gmail.com>
2025-03-17 02:19:57 +0600

    leetcode 45

--- src/DummyClass1.java
+++ src/DummyClass1.java
@@ -2,46 +2,69 @@ import java.util.Arrays;
 
 public class DummyClass1 {
     public static void main(String[] args) {
-//        int [] nums = {2,0};
+//        int [] nums = {1,2};
 //        int [] nums = {0,2,3};
-//        int [] nums = {0};
-        int [] nums = {3,2,1,0,4};
-//        int [] nums = {2,3,1,1,4};
-//        int [] nums = {1,1,1,0};
+//        int [] nums = {7,0,9,6,9,6,1,7,9,0,1,2,9,0,3};
+//        int [] nums = {2,1,3,4,3,1,1,5,6,7};
+//        int [] nums = {3,1,3,4,3,1,1,5,6,7};
+//        int [] nums = {3,1,3,1};
+        int [] nums = {2,3,1,1,4};
+//        int [] nums = {2,3,0,1,4};
 
-        boolean jump = canJump2(nums);
-        System.out.println("jump : "+ jump);
+        int jumps = jump3(nums);
+        System.out.println("jumps : "+ jumps);
 
     }
-    public static boolean canJump2(int[] nums) {
-        int jumpLen = nums[0];
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
+    public static int jump3(int[] nums) {
 
-            if (jumpLen>=n-1 || nums[i]==n-1) return true;
-            if (i==jumpLen && i<n-1 && nums[i]==0) return false;
+        int jumps=0,l=0,farthest = 0;
 
-
-            if (i+nums[i] > jumpLen){
-                jumpLen =i+nums[i];
+        for (int r = 0; r < nums.length-1; r++) {
+            farthest = Math.max(farthest,r+nums[r]);
+            if (r==l){
+                jumps++;
+                l=farthest;
             }
         }
 
-        return false;
+        return jumps;
     }
-    public static boolean canJump(int[] nums) {
-        int jumpLen = nums[0];
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
+    public static int jump2(int[] nums) {
+        int n= nums.length; if (n==1) return 0;
 
-            if (jumpLen>=n-1) return true;
-            if (i>jumpLen) return false;
-            if (i+nums[i] > jumpLen){
-                jumpLen =i+nums[i];
+        int l=0,r=nums[0], farthest = nums[0],jumps=0;
+        if (nums[l]>=n-1) {jumps++;return jumps;}
+
+        while (l<n-1){
+            l++;
+            if (l+nums[l]>farthest){
+                farthest = l+nums[l];
+            }
+            if (l==r){
+                jumps++;
+                r = farthest;
+            }
+            if (r>=n-1){ jumps++;break;}
+        }
+
+        return jumps;
+    }
+
+    public static int jump(int[] nums) {
+        int n= nums.length; if (n==1) return 0;
+
+        int p=0,farthest = nums[0];
+
+        for (int i = p+1; i < n; i++) {
+            if (i+nums[i] >= n-1) break;
+
+            if (i+nums[i]>farthest){
+                farthest = i+nums[i];
+                p = i;
             }
         }
 
-        return false;
+        return p;
     }
 
 
