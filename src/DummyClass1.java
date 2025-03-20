b35953a737cc365cb2b1ce157632053105a6aebe
kamrul <kamrul4h3190@gmail.com>
2025-03-20 21:34:13 +0600

    leetcode 151

--- src/DummyClass1.java
+++ src/DummyClass1.java
@@ -3,24 +3,14 @@ import java.util.Arrays;
 public class DummyClass1 {
     public static void main(String[] args) {
 
-        int num = 2837;
-//        int num = 3749;
-        String roman = intToRoman(num);
-        System.out.println("roman : "+ roman);
+        String s = "  hello  world  ";
+        String reversed = reverseWords(s);
+        System.out.println(reversed);
     }
-
-    public static String intToRoman(int num) {
-        int[] values =    {1000,900,500,400,100,  90, 50,  40, 10,  9,  5,  4,    1};
-        String[] romans = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-        StringBuilder roman = new StringBuilder();
-        for (int i = 0; i < values.length;i++) {
-            if (num<=0) break;
-            while (num>=values[i]){
-                roman.append(romans[i]);
-                num-=values[i];
-            }
-        }
-        return roman.toString();
+    public static String reverseWords(String s) {
+        String[] words = s.split(" +");
+        StringBuilder reversed = new StringBuilder();
+        for (int i = words.length-1; i >=0 ; i--) reversed.append(words[i]).append(" ");
+        return reversed.toString().trim();
     }
-
 }
