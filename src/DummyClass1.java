a5de8631e1c8fa46562cffc68553ac037e779cb5
kamrul <kamrul4h3190@gmail.com>
2025-03-25 22:16:59 +0600

    leetcode 135

--- src/DummyClass1.java
+++ src/DummyClass1.java
@@ -3,26 +3,65 @@ import java.util.Arrays;
 public class DummyClass1 {
     public static void main(String[] args) {
 
-        String s = "PAYPALISHIRING";int numRows =4;
-        String output = convert(s,numRows);
-        System.out.println("output :"+output);
+//        int[] ratings = {1,2,4,2,1};
+//        int[] ratings = {1,2,3,3,1,2,1};
+//        int[] ratings = {1,3,2,2,1};
+//        int[] ratings = {1,2,5,7,8,4,2,5,2,2};
+//        int[] ratings = {1,0,2};
+//        int[] ratings = {1,2,2};
+        int[] ratings = {0,2,4,7,6,5,4,3,2,1,1,1,2,3,4,2,1,1,1};
+        int candy = candy2(ratings);
+        System.out.println("candy :"+candy);
     }
-    public static String convert(String s, int numRows) {
-        if (numRows ==1 || numRows>= s.length()) return s;
 
-        StringBuilder[] rows = new StringBuilder[numRows];
-        for (int i = 0; i < numRows; i++) {rows[i] = new StringBuilder();}
-
-        boolean goingDown = false;
-        int currRow = 0;
-        for (char c : s.toCharArray()) {
-            rows[currRow].append(c);
-            if (currRow == 0 || currRow == numRows-1) goingDown =!goingDown;
-            currRow += goingDown ? 1 : -1;
+    //time-o(n), space-o(1)
+    public static int candy2(int[] ratings) {
+        int n=ratings.length,candies=1;
+        int i=1;
+        while (i < n) {
+            int up=1,down=0;
+            if (ratings[i]==ratings[i-1]){
+                i++;
+                candies++;
+                continue;
+            }
+            while (i<n && ratings[i]>ratings[i-1]){
+                up++;
+                candies+=up;
+                i++;
+            }
+            while (i<n && ratings[i]<ratings[i-1]){
+                down++;
+                candies+=down;
+                i++;
+            }
+            down++;
+//            System.out.println("up,down:"+up+","+down);
+            if ( down>up){
+                candies+= down-up;
+            }
         }
+        return candies;
+    }
 
-        StringBuilder converted = new StringBuilder();
-        for (StringBuilder row : rows) {converted.append(row);}
-        return converted.toString();
+    //time-o(n), space - o(n)
+    public static int candy(int[] ratings) {
+        int n=ratings.length;
+        int[] candies = new int[n];
+        Arrays.fill(candies,1);
+
+        for (int i = 1; i < n; i++) {
+            if (ratings[i]>ratings[i-1] && candies[i]<=candies[i-1])
+                candies[i] = candies[i-1]+1;
+        }
+        System.out.println(Arrays.toString(candies));
+
+        for (int i = n-2; i >=0 ; i--) {
+            if (ratings[i]>ratings[i+1] && candies[i]<=candies[i+1])
+                candies[i] = candies[i+1]+1;
+        }
+        System.out.println(Arrays.toString(candies));
+
+        return Arrays.stream(candies).sum();
     }
 }
