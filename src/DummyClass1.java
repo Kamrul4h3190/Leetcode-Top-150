b7285f5be0c5d25392edb77e2f0f3d7c9bb0f133
kamrul <kamrul4h3190@gmail.com>
2025-03-31 12:45:39 +0600

    leetcode 68

--- src/DummyClass1.java
+++ src/DummyClass1.java
@@ -1,63 +1,74 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class DummyClass1 {
     public static void main(String[] args) {
-//        int[] height = {4,2,0,3,2,5};
-//        int[] height = {4,2,3};
-        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
-        int water = trap2(height);
-        System.out.println("water :"+water);
+//        String[] words = {"This", "is", "an", "example", "of", "text", "justification.",};int maxWidth = 16;
+//        String[] words = {"Science","is","what","we","understand","well","enough","to","explain","to","a","computer.","Art","is","everything","else","we","do"};int maxWidth = 20;
+//        String[] words = {"What","must","be","acknowledgment","shall","be"};int maxWidth = 16;
+        String[] words = {"ask","not","what","your","country","can","do","for","you","ask","what","you","can","do","for","your","country"};int maxWidth = 16;
+//        String[] words = {"Listen","to","many,","speak","to","a","few."};int maxWidth = 6;
+        List<String> lines = fullJustify(words,maxWidth);
+        System.out.println(lines);
+//        String justifiedLine = lineJustify(words,0,3,8,maxWidth);
+//        String justifiedLine = lineJustify(words,0,3,8,maxWidth);
+//        System.out.println(justifiedLine);
+//        System.out.println("len:"+justifiedLine.length());
     }
-    //time-O(n),space-O(1)
-    public static int trap2(int[] height) {
-        int water = 0,left = 0,right = height.length-1;
-        int leftMax=height[0],rightMax=height[height.length-1];
-        while (left<right){
-
-            if (height[left] > leftMax) leftMax = height[left];
-            if (height[right] > rightMax) rightMax = height[right];
-
-            if (height[left]<=rightMax){
-                water+= leftMax-height[left];
+    public static List<String> fullJustify(String[] words, int maxWidth) {
+        ArrayList<String> lines = new ArrayList<>();
+        int n = words.length,left=0,right=0,lineLength=0,charCount=0;
+        while (right<n){
+            if (lineLength==0 && words[right].length()==maxWidth){
+                lines.add(words[right]);
                 left++;
             }
-            else {
-                water+= rightMax-height[right];
-                right--;
+            else if (lineLength+words[right].length()>=maxWidth){
+                String line = lineJustify(words,left,right,charCount,maxWidth);
+                lines.add(line);
+                lineLength=0;charCount=0;
+
+                left=right;continue;
+            } else if (lineLength<maxWidth && right==n-1) {
+                StringBuilder line = new StringBuilder();
+                while (left<=right){
+                    line.append(words[left]);if (left!=right) line.append(" ");
+                    left++;}
+                if (line.length()<maxWidth)line.append(" ".repeat(maxWidth-line.length()));
+                System.out.println("len:"+line.length());
+                lines.add(line.toString());
+            } else {
+                charCount+=words[right].length();
+                lineLength+=words[right].length();
+                if (right-left>0)lineLength++;
             }
+            right++;
         }
 
-        return water;
+        return lines;
     }
 
+public static String lineJustify(String[] words,int left, int right,int charCount,int maxWidth){
+    StringBuilder line = new StringBuilder();
 
-    //time-O(n),space-O(n)
-    public static int trap(int[] height) {
-        int water = 0,n= height.length;
+    int spaceBlocks = right-left-1,spaceCount=maxWidth-charCount;
+    int normalSpace = spaceBlocks==0? 0: spaceCount/spaceBlocks;
+    int extraSpace = spaceBlocks==0? 0: spaceCount%spaceBlocks;
 
-        int[] left = new int[n];int leftMax=0;
-        for (int i = 0; i < n; i++) {
-            if (height[i]>leftMax){
-                leftMax=height[i];
-            }
-            left[i]=leftMax;
+    for (int i = left; i < right; i++) {
+        line.append(words[i]);
+        if (extraSpace==0 && normalSpace>0 && i!=right-1){
+            line.append(" ".repeat(normalSpace));
+        } else if (extraSpace>0 && normalSpace>0 && i!=right-1) {
+            line.append(" ".repeat(normalSpace));
+            line.append(" ");
+            extraSpace--;
         }
-//        System.out.println(Arrays.toString(left));
-
-        int[] right = new int[n];int rightMax=0;
-        for (int i = n-1; i >= 0; i--) {
-            if (height[i]>rightMax){
-                rightMax=height[i];
-            }
-            right[i]=rightMax;
-        }
-//        System.out.println(Arrays.toString(right));
-
-        for (int i = 0; i < n; i++) {
-            int surface = Math.min(left[i],right[i]);
-            water+= surface-height[i];
-        }
-
-        return water;
+    }
+    if (line.length()<maxWidth)line.append(" ".repeat(maxWidth-line.length()));
+    System.out.println("len:"+line.length());
+    return line.toString();
     }
 }
+
