952bdca25685cc42c59bab331c49efdd2d5f280b
kamrul <kamrul4h3190@gmail.com>
2025-04-15 04:33:34 +0600

    leetcode 205

--- src/DummyClass1.java
+++ src/DummyClass1.java
@@ -3,20 +3,27 @@ import java.util.*;
 public class DummyClass1 {
     public static void main(String[] args) {
 //        String ransomNote = "aa", magazine = "ab";
-        String ransomNote = "aa", magazine = "aab";
-        System.out.println("canConstruct : " + canConstruct(ransomNote,magazine));
+//        String s = "paper", t = "title";
+//        String s = "eggs", t = "addd";
+//        String s = "foo", t = "bar";
+        String s = "badc", t = "baba";
+        System.out.println("isIsomorphic : " + isIsomorphic(s,t));
     }
-    public static boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character,Integer> magMap = new HashMap<>();
-        for (Character ch : magazine.toCharArray()) {magMap.put(ch, magMap.getOrDefault(ch,0)+1);}
 
-        for (char ch : ransomNote.toCharArray()){
-            if (!magMap.containsKey(ch)) return false;
-            if (magMap.get(ch) < 1) return false;
-
-            magMap.put(ch, magMap.get(ch)-1);
+    public static boolean isIsomorphic(String s, String t) {
+        HashMap<Character, Character> smap = new HashMap<>();
+        HashSet<Character> tset = new HashSet<>();
+        for (int i = 0; i <s.length() ; i++) {
+            char sSymbol = s.charAt(i);
+            char tSymbol = t.charAt(i);
+            if (!smap.containsKey(sSymbol)){
+                smap.put(sSymbol,tSymbol);
+                if (tset.contains(tSymbol))
+                    return false;
+                tset.add(tSymbol);
+            }
+            else if (tSymbol!= smap.get(sSymbol)) return false;
         }
-
         return true;
     }
 
