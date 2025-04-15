694b1cbdcab734347699248284ff29e2c33422e3
kamrul <kamrul4h3190@gmail.com>
2025-04-15 19:47:58 +0600

    leetcode 242

--- src/DummyClass1.java
+++ src/DummyClass1.java
@@ -5,11 +5,21 @@ public class DummyClass1 {
 //        String ransomNote = "aa", magazine = "ab";
 //        String s = "paper", t = "title";
 //        String s = "eggs", t = "addd";
-//        String s = "foo", t = "bar";
+//        String s = "anagram", t = "nagaram";
+        String s = "rat", t = "tac";
+//        String s = "ab", t = "a";
 //        String pattern = "abba", s = "dog cat cat dog";
 //        String pattern = "aaaa", s = "dog cat cat cat";
-        String pattern = "aaa", s = "aa aa aa aa";
-        System.out.println("wordPattern : " + wordPattern(pattern,s));
+//        String pattern = "aaa", s = "aa aa aa aa";
+        System.out.println("isAnagram : " + isAnagram(s,t));
+    }
+    public static boolean isAnagram(String s, String t) {
+        if  (s.length()!=t.length()) return false;
+        HashMap<Character,Integer> smap = new HashMap<>(),tmap = new HashMap<>();
+        for (char ch : s.toCharArray()) {smap.put(ch, smap.getOrDefault(ch,0)+1);}
+        for (char ch : t.toCharArray()) {tmap.put(ch, tmap.getOrDefault(ch,0)+1);}
+
+        return smap.equals(tmap);//entire two map object comparison
     }
     public static boolean wordPattern(String pattern, String s) {
         String[] words = s.split(" ");
