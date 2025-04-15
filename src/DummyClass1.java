e8af6e1842935adafe62e6c2186c2869bbe0c567
kamrul <kamrul4h3190@gmail.com>
2025-04-15 23:29:09 +0600

    leetcode 49

--- src/DummyClass1.java
+++ src/DummyClass1.java
@@ -2,57 +2,49 @@ import java.util.*;
 
 public class DummyClass1 {
     public static void main(String[] args) {
-//        String ransomNote = "aa", magazine = "ab";
-//        String s = "paper", t = "title";
-//        String s = "eggs", t = "addd";
-//        String s = "anagram", t = "nagaram";
-        String s = "rat", t = "tac";
-//        String s = "ab", t = "a";
-//        String pattern = "abba", s = "dog cat cat dog";
-//        String pattern = "aaaa", s = "dog cat cat cat";
-//        String pattern = "aaa", s = "aa aa aa aa";
-        System.out.println("isAnagram : " + isAnagram(s,t));
+//        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        String[] strs = {"cab","tin","pew","duh","may","ill","buy","bar","max","doc"};
+        //[["max"],["buy"],["doc"],["may"],["ill"],["duh"],["tin"],["bar"],["pew"],["cab"]]
+        List<List<String>> groups = groupAnagrams(strs);
+        System.out.println(groups);
     }
-    public static boolean isAnagram(String s, String t) {
-        if  (s.length()!=t.length()) return false;
-        HashMap<Character,Integer> smap = new HashMap<>(),tmap = new HashMap<>();
-        for (char ch : s.toCharArray()) {smap.put(ch, smap.getOrDefault(ch,0)+1);}
-        for (char ch : t.toCharArray()) {tmap.put(ch, tmap.getOrDefault(ch,0)+1);}
+    public static List<List<String>> groupAnagrams2(String[] strs) {
+        HashMap<String,List<String>> groupMap = new HashMap<>();
+        for (String word : strs) {
+            String key = generateKey(word);
 
-        return smap.equals(tmap);//entire two map object comparison
-    }
-    public static boolean wordPattern(String pattern, String s) {
-        String[] words = s.split(" ");
-        if (pattern.length()!= words.length) return false;
-        HashMap<Character,String> patternMap = new HashMap<>();
-        HashSet<String> wordSet = new HashSet<>();
-        for (int i = 0; i < pattern.length(); i++) {
-            char pSymbol = pattern.charAt(i);
-            String sWord = words[i];
-            if (!patternMap.containsKey(pSymbol)){
-                patternMap.put(pSymbol,sWord);
-                if (wordSet.contains(sWord)) return false;
-                wordSet.add(sWord);
-            } else if (!sWord.equals(patternMap.get(pSymbol))) return false;
-        }
-        return true;
-    }
-
-    public static boolean isIsomorphic(String s, String t) {
-        HashMap<Character, Character> smap = new HashMap<>();
-        HashSet<Character> tset = new HashSet<>();
-        for (int i = 0; i <s.length() ; i++) {
-            char sSymbol = s.charAt(i);
-            char tSymbol = t.charAt(i);
-            if (!smap.containsKey(sSymbol)){
-                smap.put(sSymbol,tSymbol);
-                if (tset.contains(tSymbol))
-                    return false;
-                tset.add(tSymbol);
+            if (!groupMap.containsKey(key)) {
+                groupMap.put(key, new ArrayList<>());;
             }
-            else if (tSymbol!= smap.get(sSymbol)) return false;
+            groupMap.get(key).add(word);
         }
-        return true;
+        return  new ArrayList<>(groupMap.values()) ;
+    }
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>> groupMap = new HashMap<>();
+        for (String word : strs) {
+            char[] letters = word.toCharArray();Arrays.sort(letters);
+            String key = new String(letters);
+            if (!groupMap.containsKey(key)) {
+                groupMap.put(key, new ArrayList<>());;
+            }
+            groupMap.get(key).add(word);
+        }
+        return  new ArrayList<>(groupMap.values()) ;
+    }
+
+    private static String generateKey(String word){
+        int[] freqCount = new int[26];
+        for (char letter : word.toCharArray()) {
+            freqCount[letter-'a']++;
+        }
+
+        StringBuilder key = new StringBuilder();
+        for (int j : freqCount) {
+            if (j != 0) key.append(Arrays.toString(freqCount));
+            else key.append("*");
+        }
+        return key.toString();
     }
 
 }
