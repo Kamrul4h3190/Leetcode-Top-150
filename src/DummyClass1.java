bdb3b35417630065f58f1b4326730c5ed64d8658
kamrul <kamrul4h3190@gmail.com>
2025-04-20 02:43:01 +0600

    leetcode 20

--- src/DummyClass1.java
+++ src/DummyClass1.java
@@ -4,23 +4,33 @@ import static java.lang.Integer.compare;
 
 public class DummyClass1 {
     public static void main(String[] args) {
-        int[][] points = {{10,16},{2,8},{1,6},{7,12}};
-//        int[][] points = {{-46,-45},{46,47}};
-//        int[][] points = {{-2147483646,-2147483645},{2147483646,2147483647}};
-//        int[][] points = {{3,9},{7,12},{3,8},{6,8},{9,10},{2,9},{3,9},{0,6},{2,8}};
-        System.out.println("arrows : "+findMinArrowShots(points));
+//        String s = "()[]{}";
+//        String s = "(]";
+//        String s = "([])";
+        String s = "))";
+        System.out.println("valid parentheses : "+isValid(s));
     }
-    public static int findMinArrowShots(int[][] points) {
-        if (points.length<=1) return points.length;
-        Arrays.sort(points,(a,b)->Integer.compare(a[1],b[1]));
+    public static boolean isValid(String s) {
+        int n = s.length();if (n%2 != 0 ) return false;
 
-        int arrows = 1,boundary = points[0][1];
-        for (int i = 1; i < points.length ; i++) {
-            if(points[i][0]>boundary){
-                arrows++;boundary=points[i][1];
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c=='(' || c=='{' || c=='[') stack.push(c);
+            else if (!stack.isEmpty()){
+                switch (c){
+                    case ')': if (stack.peek()!='(')
+                        return false;break;
+                    case '}': if (stack.peek()!='{')
+                        return false;break;
+                    case ']': if (stack.peek()!='[')
+                        return false;break;
+                }
+                stack.pop();
             }
+            else return false;
         }
-        return arrows;
+
+        return stack.isEmpty();
     }
 
 }
