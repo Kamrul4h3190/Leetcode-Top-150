cfe3ab02311d3fabf072bd5274ddab9e45b220c0
kamrul <kamrul4h3190@gmail.com>
2025-04-24 00:50:14 +0600

    leetcode 141

--- src/TestClass.java
+++ src/TestClass.java
@@ -5,35 +5,45 @@ import java.util.Stack;
 
 public class TestClass {
     public static void main(String[] args) {
-        String s = "(1+(4+5+2)-3)+(6+8)";
-        System.out.println("value : "+calculate(s));
-    }
-    public static int calculate(String s) {
-        int res=0,num=0,sign=1;
-        Stack<Integer> stack = new Stack<>();
-        for (char c : s.toCharArray()) {
-            if (Character.isDigit(c)){
-                num = 10*num + (c-'0');
-            } else if (c=='+') {
-                res += num*sign;
-                num=0;sign=1;
-            }else if (c=='-') {
-                res += num*sign;
-                num=0;sign=-1;
-            }else if (c=='(') {
-                stack.push(res);stack.push(sign);
-                res=0;num=0;sign=1;
-            } else if (c==')') {
-                res += num*sign;
-                res *= stack.pop(); //sign outside the parentheses
-
-                res+=stack.pop();
-                num=0;sign=1;
-            }
+        int[] nums = {3,2,0,-4};int pos = 1;
+        ListNode head = new ListNode(nums[0]);  ListNode curr = head;ListNode tail = head;   ListNode joint = null;
+        for (int i = 1; i < nums.length ; i++) {
+            tail = new ListNode(nums[i]);
+            if (i==pos) joint = tail;
+            curr.next  = tail;
+            curr = tail;
         }
-        res+=num*sign;
-        return res;
+        tail.next=joint; //adding the cycle
+
+        System.out.println("has cycle : "+hasCycle(head));
+    }
+    public static boolean hasCycle(ListNode head) {
+        ListNode slow = head,fast= head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow==fast) return true;
+        }
+        return false;
+    }
+
+    public static  void printList(ListNode head){
+        while (head!=null){
+            System.out.print(head.val); if (head.next!=null) System.out.print("->");
+            head = head.next;
+        }
     }
 
 }
+
+
+  class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
+
 
