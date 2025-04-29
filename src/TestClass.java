bcd4b4d8f51b0bc0c7a7fbdbe2c03ca14e345933
kamrul <kamrul4h3190@gmail.com>
2025-04-30 01:37:59 +0600

    leetcode 61

--- src/TestClass.java
+++ src/TestClass.java
@@ -3,26 +3,54 @@ import java.util.List;
 
 public class TestClass {
     public static void main(String[] args) {
-        int[] nodes = {1,2,3,3,4,4,5};
+        int[] nodes = {1,2,3,4,5}; int k = 5;
+//        int[] nodes = {0,1,2}; int k = 4;
+//        int[] nodes = {1,2,3}; int k = 2000000000;
         ListNode head = new ListNode(nodes[0]); ListNode tail = head;
         buildList(nodes, tail);
-        head = deleteDuplicates(head);
+        head = rotateRight2(head,k);
         printList(head);
     }
-    public static ListNode deleteDuplicates(ListNode head) {
-        if (head == null || head.next == null) return head;
+    public static ListNode rotateRight2(ListNode head, int k) {
+        if(head==null || head.next ==null || k==0) return head;
         ListNode dummyNode = new ListNode(-1,head);
-        ListNode prev = dummyNode,curr = head;
-        while (curr!=null){
-            if (curr.next!=null && curr.next.val == curr.val){
-                while (curr.next!=null && curr.next.val == curr.val) curr = curr.next;
-                prev.next = curr.next;
-            }
-            else prev = prev.next;
+        ListNode left = dummyNode,right = dummyNode;
 
-            curr = curr.next;
+        int len = 1;ListNode curr = head;
+        while (curr.next!=null){curr = curr.next;len++;}
+
+        k = k%len; if (k==0) return head;
+
+        for (int i = 1; i <=k ; i++) {right = right.next;}
+        if (right.next==null) return head;
+
+        while (right.next!=null){
+            left = left.next;
+            right = right.next;
         }
 
+        right.next = dummyNode.next;
+        dummyNode.next = left.next;
+        left.next = null;
+        return dummyNode.next;
+    }
+    public static ListNode rotateRight(ListNode head, int k) {
+        if(head==null || head.next ==null || k==0) return head;
+        ListNode dummyNode = new ListNode(-1,head);
+        ListNode left = dummyNode,right = dummyNode;
+        for (int i = 1; i <=k ; i++) {
+            right = right.next;
+            if (right==null) right = dummyNode.next; //cycle here, Time complexity becomes O(K).
+        }
+        if (right.next==null) return head;
+
+        while (right.next!=null){
+            left = left.next;
+            right = right.next;
+        }
+        right.next = dummyNode.next;
+        dummyNode.next = left.next;
+        left.next = null;
         return dummyNode.next;
     }
 
