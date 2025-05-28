be12ffcb6c4eb4d165e8c3a16088928abb27bb9f
kamrul <kamrul4h3190@gmail.com>
2025-05-29 05:51:58 +0600

    leetcode 433

--- src/TestClass.java
+++ src/TestClass.java
@@ -4,53 +4,40 @@ public class TestClass {
 
     public static void main(String[] args) {
         TestClass testClass = new TestClass();
-        int[][] board = {
-                {-1,-1,-1,-1,-1,-1},
-                {-1,-1,-1,-1,-1,-1},
-                {-1,-1,-1,-1,-1,-1},
-                {-1,35,-1,-1,13,-1},
-                {-1,-1,-1,-1,-1,-1},
-                {-1,15,-1,-1,-1,-1}
-        };
+        String startGene = "AACCGGTT", endGene = "AAACGGTA";
+        String[] bank = {"AACCGGTA","AACCGCTA","AAACGGTA"};
 
-        System.out.println("minMoves : "+testClass.snakesAndLadders(board));
+        System.out.println("minMoves : "+testClass.minMutation(startGene,endGene,bank));
     }
-    public int snakesAndLadders(int[][] board) {
-        int moves = 0,n = board.length;
-        Queue<Integer> queue = new LinkedList<>();
-        boolean[][] visited = new boolean[n][n];
+    public int minMutation(String startGene, String endGene, String[] bank) {
+        int level = 0;
+        HashSet<String> bankSet = new HashSet<>(List.of(bank));
 
-        queue.add(1); visited[n-1][0] = true;
+        Queue<String> queue = new LinkedList<>(); HashSet<String> visited = new HashSet<>();
+        queue.offer(startGene); visited.add(startGene);
         while (!queue.isEmpty()){
-            int levelSize = queue.size();
-            for (int i = 0; i < levelSize; i++) {
-                int currVal = queue.poll();
-                if (currVal==n*n) return moves;
-
-                for (int dice = 1; dice <=6 ; dice++) {
-                    if (currVal+dice==n*n) return moves+1;
-
-                    int[] boustrophedonPosition = findCoordinates(currVal+dice,n);
-                    int x = boustrophedonPosition[0],y = boustrophedonPosition[1];
-                    if (!visited[x][y]){
-                        visited[x][y] = true;
-
-                        if (board[x][y]==-1)    queue.add(currVal+dice);
-                        else {
-                            if (board[x][y]==n*n) return moves+1;
-                            queue.add(board[x][y]);
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String curr = queue.poll();
+                if (curr.equals(endGene)) return level;
+                
+                for (char gene : "ACGT".toCharArray()) {
+                    for (int s = 0; s < 8; s++) {
+                        char[] currCharArray = curr.toCharArray();
+                        if(currCharArray[s]!=gene){
+                            currCharArray[s] = gene;
+                            String next = new String(currCharArray);
+                            if (!visited.contains(next) && bankSet.contains(next)){
+                                queue.add(next);
+                                visited.add(next);
+                            }
                         }
                     }
                 }
             }
-            moves++;
+            level++;
         }
+
         return -1;
-    }
-    public int[] findCoordinates(int curr, int n) {
-        int r = (curr - 1) / n;
-        int c = (curr - 1) % n;
-        if (r % 2 != 0) c = n-1-c;
-        return new int[]{n-1-r, c};
     }
 }
